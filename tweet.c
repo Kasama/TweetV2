b462914e26f9d9c408ca8d903c045eab18b0933a
fredmarques <frederico.marques@live.com>
2015-06-08 20:13:58 -0300

    finished writeTweet, need bug fix

--- tweet.c
+++ tweet.c
@@ -4,6 +4,7 @@
 #include "tweet.h"
 
 #define HEADER (sizeof(int))	// Size of the data before the first tweet
+#define minTweetSize  (4 + 2*sizeof(int) + sizeof(long))
 
 struct tweet{
 
@@ -16,32 +17,105 @@ struct tweet{
 	long  viewsCount;
 
 };
-static long bestFit(char *filename, long tweetSize){
-	long offset, currentDifference, stackPos;
-	
-	FILE *arq = fopen(filename, "r");
 
+static long bestFit(char* filename, long tweetSize, long *minPreviusOffset){
+
+	long offset, stackPos, currentPreviusOffset;
+	int currentDifference, minDifference, blockSize;
+	*minPreviusOffset = 0;
+
+	FILE *arq = fopen(filename, "r");
 	fread(&stackPos, sizeof(long), 1, arq);
+
+	if(stackPos <= 0){
+		fclose(arq);
+		return 0;
+	}
+
+	fseek(arq, stackPos, SEEK_SET);
+	fread(&minDifference, sizeof(int), 1, arq);
+	minDifference *= -1;
+	currentPreviusOffset = stackPos;
+	fread(&stackPos, sizeof(long), 1, arq);
+
+	while(stackPos != -1 && minDifference >= 0){
+		//currentPreviusOffset = stackPos;
+		fseek(arq, stackPos, SEEK_SET);
+		fread(&blockSize, sizeof(int), 1, arq);
+		blockSize *= -1;
+		currentDifference = blockSize - tweetSize;
+
+		if(currentDifference >= 0 && currentDifference < minDifference){
+			minDifference = currentDifference;
+			*minPreviusOffset = currentPreviusOffset;
+		}
+		currentPreviusOffset = stackPos;	
+		fread(&stackPos, sizeof(long), 1, arq);
+	}
+
+	fclose(arq);
+	return offset;	
+}
+static long tweetSize(TWEET* tweet){
+	long sum = 0;
+	sum += strlen(tweet->text);
+	sum += strlen(tweet->userName);
+	sum += strlen(tweet->coords);
+	sum += strlen(tweet->language);
+	sum += 2*sizeof(int) + sizeof(long);
+	
+	return sum;
 }
 
+static void flushTweet(char* filename, long byteOffset, long fieldsize, TWEET *tweet){
+
+	FILE *arq = NULL;
+	if(byteOffset == 0){
+		arq = fopen(filename, "a");
+	}else{
+		arq = fopen(filename, "w+");
+		fseek(arq, byteOffset, SEEK_SET);
+	}
+	fwrite(&fieldsize, sizeof(int), 1, arq);
+	fwrite(tweet->text, sizeof(int), 1, arq);
+	fwrite(tweet->userName, sizeof(int), 1, arq);
+	fwrite(tweet->coords, sizeof(int), 1, arq);
+	fwrite(tweet->language, sizeof(int), 1, arq);
+	
+	fclose(arq);
+}
 void writeTweet(char *filename, TWEET *tweet){
-/*
- * Writes the tweet into the file
- * 
- * byteOffset = bestFit(tweet, *regSize)
- * fwrite(sizeof(tweet), byteOffset);
- * fwrite(tweet, byteOffset+sizeof(int))
- * if ((regSize-sizeof(tweet)) >= sizeof(int)){
- *   fwrite(-(regSize-sizeof(tweet)), byteOffset+sizeof(int)+sizeof(tweet))
- * }
- *
- */
-	//check for invalid inputs
+	if(filename == NULL || tweet  == NULL)	
+		return;
+
+	long tweetsize = tweetSize(tweet);
+	long previusOffset, previusOffsetValue;
+	long byteOffset  = bestFit(filename, tweetsize, &previusOffset);
 	if(filename == NULL || tweet == NULL)
 		return;
+	FILE *arq = fopen(filename, "w+");
 	
-
-	return;
+	int fieldSize;
+	fread(&fieldSize, sizeof(int), 1, arq);
+	fieldSize *= -1;
+	
+	long newFieldSize = fieldSize - (tweetsize + sizeof(int));
+	if(byteOffset == 0){
+		flushTweet(filename, byteOffset, tweetsize, tweet);
+	}
+	else if(newFieldSize < minTweetSize){
+		fseek(arq, byteOffset + sizeof(int), SEEK_SET);
+		fread(&previusOffsetValue, sizeof(long), 1, arq);
+		fseek(arq, previusOffset + sizeof(int), SEEK_SET);
+		fwrite(&previusOffsetValue, sizeof(long), 1, arq);
+		flushTweet(filename, byteOffset, fieldSize, tweet);
+	}else{
+		newFieldSize *= -1;			
+		fseek(arq, byteOffset, SEEK_SET);
+		fwrite(&newFieldSize, sizeof(int), 1, arq);
+		byteOffset += sizeof(int) + newFieldSize;
+		flushTweet(filename, byteOffset, tweetsize, tweet);
+	}
 }
 
 TWEET *readTweet(char *filename, int offset){
